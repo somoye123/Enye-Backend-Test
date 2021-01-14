3210f3cde714eecd7d67d49d68606ef7780b32e2
somoye123 <somoye.ayotunde@gmail.com>
2021-01-14 14:30:25 +0100

    add cors, express and dotenv dependencies

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 /*
  * Primary file for the API
  */
+
+//  Dependencies
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import RateRoute from './routes/rateRoute';
