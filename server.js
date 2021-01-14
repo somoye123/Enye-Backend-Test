4c2e630db97fc44f995ff1f0f85f4976d7838a6c
somoye123 <somoye.ayotunde@gmail.com>
2021-01-14 14:31:43 +0100

    instantiate express server and invoke dotenv

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import RateRoute from './routes/rateRoute';
+
+dotenv.config();
+
+// Instantiate the server
+const app = express();
